fc3e31accacb10a6a1705f43f069e15bdf78a36d
Daniel Mellgaard Frost <danielfrostdk@outlook.com>
2021-03-17 13:31:08 +0100

    init

---
+++ MoveFilesTask/MoveFilesTask/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoveFilesTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+        }
+    }
+}
